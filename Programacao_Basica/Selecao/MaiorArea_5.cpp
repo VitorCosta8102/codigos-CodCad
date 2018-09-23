7aae2d93895a9d8b9d6244bef5bf51b4d734c467
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-09-23 09:37:19 -0300

    Inicio da separacao por pastas

---
+++ Programacao_Basica/Selecao/MaiorArea_5.cpp
@@ -0,0 +1,17 @@
+//---------------------------------
+//			  CodCad
+//  Programação Basica: Seleção
+// 	  Problema 5: Maior área
+//---------------------------------
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int L1,A1,L2,A2;
+	cin>>L1>>A1>>L2>>A2;
+	if((L1*A1)>(L2*A2)) cout<<"Primeiro"<<endl;
+	else if((L1*A1)<(L2*A2)) cout<<"Segundo"<<endl;
+	else cout<<"Empate"<<endl;
+	return 0;
+}
---
+++ Programacao_Basica/Selecao/MelhorAluno_4.cpp
@@ -0,0 +1,17 @@
+//---------------------------------
+//			 CodCad
+// 	Programação Basica: Seleção
+// 	 Problema 4: Melhor aluno
+//---------------------------------
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	double a,b;
+	cin>>a>>b;
+	if(a>b) cout<<"Paulo"<<endl;
+	else cout<<"Pedro"<<endl;
+	return 0;	
+}
+
